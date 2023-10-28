4af4ef3c8fe74e248ff5656bed74534119da8257
Chakir-Aicha <aicha.chakir.2022@gmail.com>
2023-10-28 15:36:31 +0100

    commit 7

--- src/components/ListProduct.jsx
+++ src/components/ListProduct.jsx
@@ -1,7 +1,29 @@
 import image from "./produit.png";
 import {Link} from "react-router-dom";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFolder} from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 export default function ListProduct(){
+    const [typeProduits, setTypeProduits] = useState([]);
+    const [typeSelectionne, setTypeSelectionne] = useState("");
+
+    //  récupérer les types de produits
+    useEffect(() => {
+        axios
+            .get("http://localhost:8083/typeproduits")
+            .then((response) => {
+                setTypeProduits(response.data);
+            })
+            .catch((error) => {
+                console.error("Erreur lors de la récupération des types de produits :", error);
+            });
+    }, []);
+
+
+    const handleTypeSelection = (e) => {
+        setTypeSelectionne(e.target.value); // Mettre à jour le type sélectionné
+    };
     return (
         <div className="container">
             <div className="sidebar">
@@ -15,6 +37,15 @@ export default function ListProduct(){
             </div>
             <div className="content">
                 <h2>Liste des produits</h2>
+                <div>
+                    <label>Category :</label>
+                    <select className="triangle-icon" onChange={handleTypeSelection}>
+                        <option> <FontAwesomeIcon icon={faFolder} />Toutes les catégories</option>
+                        {typeProduits.map((typeProduit, index) => (
+                            <option key={index}>{typeProduit.nom}</option>
+                        ))}
+                    </select>
+                </div>
             </div>
          </div>
          );
